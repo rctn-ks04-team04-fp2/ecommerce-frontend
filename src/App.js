38a9467e594136463ada7f8e38344a224b6cdbf7
Rafli Bangsawan <rafli.bangsawan@ui.ac.id>
2022-11-12 02:25:02 +0700

    added hook for get product items

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { NavBar } from './components/NavBar.js'
 import './styles/globals.css';
 import './styles/colors.css';
+import Products from './components/Products.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Products />
+      <Footer />
     </div>
   );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useGetProduct } from '../hooks/useGetProduct'
+import { useSelector } from 'react-redux';
 
 const Products = () => {
+  const {product, loading} = useGetProduct();
+  console.log(product);
   return (
-    <div>Products</div>
+    <>
+      
+    </>
   )
 }
 
