a90ffcbe94125bb5781bdeec2459efe7050d7a1e
luthfika03 <luthfikaanggit2366@gmail.com>
2022-11-11 08:25:42 +0700

    add-login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
+import Login from './app/Login';
+import Product from './app/Product';
 
 function App() {
+  const [token, setToken] = useState(localStorage.getItem("userToken") ?? null);
+
   return (
-    <div>
-      <h1>Tes jalan</h1>
+    <div className='App'>
+      <Product />
+      {token ? <Product /> : <Login token={token} setToken={setToken} />}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import Login from './app/Login';
+import { useState } from 'react';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
 
 root.render(
   <React.StrictMode>
